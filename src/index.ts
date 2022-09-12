3e4b56ea17f76dc247ee488f55230556907e306a
Jacob Wright <jacwright@gmail.com>
2022-09-12 10:55:36 -0600

    Export compose but not all the utils at index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { applyPatch } from './applyPatch';
 export { invertPatch } from './invertPatch';
 export { transformPatch } from './transformPatch';
+export { composePatch } from './composePatch';
 export { JSONPatch } from './jsonPatch';
-export * from './utils';
 export * from './syncable';
 export * as defaultOps from './ops';
 
